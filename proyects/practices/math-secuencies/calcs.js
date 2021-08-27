134be136fa119098e721d1fda3d8694146d45d83
HurtadoAgustin <leandroagustinhurtado@gmail.com>
2021-08-27 00:19:29 -0300

    Fail in try to fix Math-secuencies

--- proyects/practices/math-secuencies/calcs.js
+++ proyects/practices/math-secuencies/calcs.js
@@ -3,29 +3,29 @@
  * Despúes se suman los anteriores
  * 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...
 */
-const fibo_in = parseInt(document.getElementById("fibonacci-input"));
-const fibo_bu = document.getElementById("fibonacci-button");
-const fibo_results = fibo_bu.addEventListener("click", fibonacci);
-function fibonacci(){
-  let fibo = [];
-  let a = b = 1; let c;
-  for (let i = 0; i < fibo_in.value; i++) {
-    (i==0 || i==1)
-    ? c=1
-    : c=a+b; a=b; b=c;
-    fibo.push(c);
+window.onload = function(){
+  document.getElementById("fibonacci-button").onclick = function(){
+    const fibo_in = document.getElementById("fibonacci-input");
+    if (fibo_in == undefined) document.write(fibo_in);
+    let fibo = [];
+    let a = b = 1; let c;
+    for (let i = 0; i < parseInt(fibo_in); i++) {
+      (i==0 || i==1)
+      ? c=1
+      : c=a+b; a=b; b=c;
+      fibo.push(c);
+    }
+    document.write(fibo);
   }
-  return fibo;
 }
+
 
 /***   Conjetura de Collatz (3N + 1)
  * Numeros impar: 3x + 1
  * Numeros par: x / 2
 */
 var coll_in = document.getElementById("collatz-input");
-var coll_bu = document.getElementById("collatz-button");
-coll_bu.addEventListener("click", collatz);
-function collatz(){
+document.querySelector("collatz-button").onclick = () => {
   let coll = [];
   let last = 19;
   if (last < 0 || last == undefined) return last;
@@ -37,8 +37,9 @@ function collatz(){
     }
     coll.push(last);
   }
-  return coll;
+  document.write(coll);
 }
+
 
 /*** Proporcion aurea
  * Es un cuadrado, que se corta a la mitad,
